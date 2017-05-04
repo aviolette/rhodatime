e78f7ccd01d4c32c73cf95965ab39f8f2dcc1d8d
Andrew Violette <aviolette@gmail.com>
2017-05-04 08:14:31 -0500

    Added with_month/with_day/with_year to date

--- lib/local_date.rb
+++ lib/local_date.rb
@@ -2,38 +2,68 @@ require 'time'
 require_relative './local_date_time'
 
 module RhodaTime
+  # A date without a timezone or time component, representing the ISO-8601 calendar.
   class LocalDate
     attr_reader :year, :month, :day
 
+    # Builds a [LocalDate] from the specified year-month-day
     def self.of(year, month, day)
       self.new(year, month, day)
     end
 
+    # Returns a [LocalDate] on the specified clock
     def self.now(clock = Clock.instance)
       self.from_epoch(clock.now)
     end
 
+    # Builds a local date from epoch time
+    # @param val [int] a millisecond representation of time from January 1, 1970
+    # @return [LocalDate] a date object representing the specified epoch time with 0 TZ offset
     def self.from_epoch(val)
       # TODO: do this natively
       time = Time.at(val / 1000).getlocal(0)
       self.new(time.year, time.month, time.day)
     end
 
+    # Returns a [LocalDateTime] object with the current date object at the start of the day
     def at_start_of_day
       LocalDateTime.of(year, month, day, 0, 0, 0, 0)
     end
 
+    # Returns a [LocalDateTime] object at the specified time
+    # @param time [LocalTime] the time of day
+    # @return [LocalDateTime] a combined date/time object
     def with_time(time)
       LocalDateTime.of(year, month, day, time.hour, time.minute, time.second, time.millis)
     end
 
+    # Returns a new [LocalDate] with a new month
+    # @param month [int] a number from 1-12
+    # @return [LocalDate] a new date object with the specified month
     def with_month(month)
       LocalDate.of(@year, month, @day)
+    end
+
+    # Returns a new [LocalDate] with a new day
+    # @param month [int] a number from 1-31
+    # @return [LocalDate] a new date object with the specified day
+    def with_day(day)
+      LocalDate.of(@year, @month, day)
+    end
+
+    # Returns a new [LocalDate] with a new year
+    # @param month [int] a number representing the year
+    # @return [LocalDate] a new date object with the specified year
+    def with_year(year)
+      LocalDate.of(year, @month, @day)
     end
 
     private
 
     def initialize(year, month, day)
+      raise DateTimeException, "Month needs a value between 1-12, but was #{month}" if month < 1 or month > 12
+      # TODO: should be tailored to months/leap years
+      raise DateTimeException, "Day needs a value between 1-31, but was #{day}" if day < 1 or day > 31
       @year = year; @month = month; @day = day
     end
   end
--- test/local_date_test.rb
+++ test/local_date_test.rb
@@ -2,57 +2,117 @@ require_relative './fake_clock'
 require_relative '../lib/rhodatime'
 require 'minitest/autorun'
 
-class TestLocalDate < Minitest::Test
+module RhodaTime
+  class TestLocalDate < Minitest::Test
 
-  def test_local_of
-    d = RhodaTime::LocalDate.of(2017, 12, 1)
-    assert_equal(2017, d.year)
-    assert_equal(12, d.month)
-    assert_equal(1, d.day)
-  end
+    ## OF
 
-  def test_local_now
-    d = RhodaTime::LocalDate.now(FakeClock.new(1483228800000))
-    assert_equal(2017, d.year)
-    assert_equal(1, d.month)
-    assert_equal(1, d.day)
-  end
+    def test_local_of
+      d = LocalDate.of(2017, 12, 1)
+      assert_equal(2017, d.year)
+      assert_equal(12, d.month)
+      assert_equal(1, d.day)
+    end
 
-  def test_local_now2
-    d = RhodaTime::LocalDate.now(FakeClock.new(1514764800000))
-    assert_equal(2018, d.year)
-    assert_equal(1, d.month)
-    assert_equal(1, d.day)
-  end
+    def test_local_now
+      d = LocalDate.now(FakeClock.new(1483228800000))
+      assert_equal(2017, d.year)
+      assert_equal(1, d.month)
+      assert_equal(1, d.day)
+    end
 
-  def test_local_now3
-    d = RhodaTime::LocalDate.now(FakeClock.new(1514757599000))
-    assert_equal(2017, d.year)
-    assert_equal(12, d.month)
-    assert_equal(31, d.day)
-  end
+    ## NOW
 
-  def test_at_start_of_day
-    d = RhodaTime::LocalDate.now(FakeClock.new(1514757599000))
-    dt = d.at_start_of_day
-    assert_equal(2017, dt.year)
-    assert_equal(12, dt.month)
-    assert_equal(31, dt.day)
-    assert_equal(0, dt.hour)
-    assert_equal(0, dt.minute)
-    assert_equal(0, dt.second)
-    assert_equal(0, dt.millis)
-  end
+    def test_local_now2
+      d = LocalDate.now(FakeClock.new(1514764800000))
+      assert_equal(2018, d.year)
+      assert_equal(1, d.month)
+      assert_equal(1, d.day)
+    end
 
-  def test_with_local_time
-    d = RhodaTime::LocalDate.of(2017, 3, 14)
-    dt = d.with_time(RhodaTime::LocalTime.of(2, 3, 4, 5))
-    assert_equal(2017, dt.year)
-    assert_equal(3, dt.month)
-    assert_equal(14, dt.day)
-    assert_equal(2, dt.hour)
-    assert_equal(3, dt.minute)
-    assert_equal(4, dt.second)
-    assert_equal(5, dt.millis)
+    def test_local_now3
+      d = LocalDate.now(FakeClock.new(1514757599000))
+      assert_equal(2017, d.year)
+      assert_equal(12, d.month)
+      assert_equal(31, d.day)
+    end
+
+    ## AT START OF DAY
+
+    def test_at_start_of_day
+      d = LocalDate.now(FakeClock.new(1514757599000))
+      dt = d.at_start_of_day
+      assert_equal(2017, dt.year)
+      assert_equal(12, dt.month)
+      assert_equal(31, dt.day)
+      assert_equal(0, dt.hour)
+      assert_equal(0, dt.minute)
+      assert_equal(0, dt.second)
+      assert_equal(0, dt.millis)
+    end
+
+    ## WITH_TIME
+
+    def test_with_local_time
+      d = LocalDate.of(2017, 3, 14)
+      dt = d.with_time(LocalTime.of(2, 3, 4, 5))
+      assert_equal(2017, dt.year)
+      assert_equal(3, dt.month)
+      assert_equal(14, dt.day)
+      assert_equal(2, dt.hour)
+      assert_equal(3, dt.minute)
+      assert_equal(4, dt.second)
+      assert_equal(5, dt.millis)
+    end
+
+    ## WITH_YEAR
+
+    def test_with_year
+      d = LocalDate.of(2018, 3, 14).with_year(2018)
+      assert_equal(2018, d.year)
+      assert_equal(3, d.month)
+      assert_equal(14, d.day)
+    end
+    ## WITH_MONTH
+
+    def test_with_month
+      d = LocalDate.of(2017, 3, 14).with_month(4)
+      assert_equal(2017, d.year)
+      assert_equal(4, d.month)
+      assert_equal(14, d.day)
+    end
+
+    def test_with_month_too_high
+      assert_raises DateTimeException do
+        LocalDate.of(2017, 3, 14).with_month(13)
+      end
+    end
+
+    def test_with_month_too_low
+      assert_raises DateTimeException do
+        LocalDate.of(2017, 3, 14).with_month(0)
+      end
+    end
+
+    ## WITH_DAY
+
+    def test_with_day
+      d = LocalDate.of(2017, 3, 14).with_day(30)
+      assert_equal(2017, d.year)
+      assert_equal(3, d.month)
+      assert_equal(30, d.day)
+    end
+
+    def test_with_day_too_high
+      assert_raises DateTimeException do
+        LocalDate.of(2017, 3, 14).with_day(32)
+      end
+    end
+
+    def test_with_day_too_low
+      assert_raises DateTimeException do
+        LocalDate.of(2017, 3, 14).with_day(0)
+      end
+    end
   end
 end
