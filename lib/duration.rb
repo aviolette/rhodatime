e86de51db223cfe02ee13e3bf651e785d48a7838
Andrew Violette <aviolette@gmail.com>
2017-05-31 20:57:03 -0500

    Beefed up the duration documentation

--- lib/duration.rb
+++ lib/duration.rb
@@ -1,12 +1,14 @@
 require 'time'
 
 module RhodaTime
+  # A duration represents elapsed time in millisecond precision
   class Duration
 
-    # add multiply_by and divide_by and * and / operators
-
+    # Builds a [Duration] from the specified number of milliseconds
+    # @param millis [int] the number of milliseconds in this duration
+    # @return [Duration] a new Duration object with the specified number of milliseconds
     def self.of_millis(millis)
-      new(millis)
+      new(millis.to_i)
     end
 
     def initialize(millis = 0)
@@ -15,68 +17,99 @@ module RhodaTime
 
     ZERO = Duration.new
 
+    # Builds a [Duration] from the specified number of seconds
+    # @param seconds [int] the number of seconds in this duration
+    # @return [Duration] a new Duration object form the specified number of seconds
     def self.of_seconds(seconds)
-      new(seconds * SECONDS)
+      new(seconds.to_i * SECONDS)
     end
 
+    # Builds a [Duration] from the specified number of minutes
+    # @param minutes [int] the number of minutes in this duration
+    # @return [Duration] a new Duration object from the specified number of minutes
     def self.of_minutes(minutes)
-      new(minutes * MINUTES)
+      new(minutes.to_i * MINUTES)
     end
 
+    # Builds a [Duration] from the specified number of hours
+    # @param hours [int] the number of hours in this duration
+    # @return [Duration] a new Duration object from the specified number of hours
     def self.of_hours(hours)
-      new(hours * HOURS)
+      new(hours.to_i * HOURS)
     end
 
+    # Builds a [Duration] from the specified number of days
+    # @param days [int] the number of days in this duration
+    # @return [Duration] a new Duration object from the specified number of days
     def self.of_days(days)
       new(days * DAYS)
     end
 
-    #TODO: include temporal / add units
-
+    # Adds minutes to the current duration
+    # @param minutes [int] the number of minutes to add to this duration
+    # @return [Duration] a new Duration object
     def plus_minutes(minutes)
-      Duration.new(@millis + (minutes * MINUTES))
+      Duration.new(@millis + (minutes.to_i * MINUTES))
     end
 
+    # Adds hours to the current duration
+    # @param hours [int] the number of hours to add to this duration
+    # @return [Duration] a new Duration object
     def plus_hours(hours)
-      Duration.new(@millis + (hours * HOURS))
+      Duration.new(@millis + (hours.to_i * HOURS))
     end
 
+    # Adds seconds to the current duration
+    # @param seconds [int] the number of seconds to add to this duration
+    # @return [Duration] a new Duration object
     def plus_seconds(seconds)
-      Duration.new(@millis + (seconds * SECONDS))
+      Duration.new(@millis + (seconds.to_i * SECONDS))
     end
 
     def plus_millis(millis)
       Duration.new(@millis + millis)
     end
 
+    # Returns the number of milliseconds in this duration
     def to_millis
       @millis
     end
 
+    # Returns a new duration with the opposite sign of the current duration
     def negated
       Duration.new(-@millis)
     end
 
+    # Returns a new duration with the same scalar value but with a positive sign
     def abs
       negative? ? Duration.of_millis(@millis.abs) : self
     end
 
+    # Returns true if the current duration has a negative value
     def negative?
       @millis < 0
     end
 
+    # Multiplies this duration by a scalar factor
+    # @param other [int] a multiplier
+    # @return a new duration with the scalar times the millisecond value
     def multiply_by(other)
       Duration.new(@millis * other)
     end
 
+    # Divides this duration by a scalar factor
+    # @param other [int] a divisor
+    # @return a new duration with the scalar divided by the other value
     def divide_by(other)
       Duration.new(@millis / other)
     end
 
+    # Same as multiply_by
     def *(other)
       multiply_by(other)
     end
 
+    # Same as divide_by
     def /(other)
       divide_by(other)
     end
@@ -105,6 +138,7 @@ module RhodaTime
       "#{sign}PT#{hours}#{minutes}#{seconds}"
     end
 
+    # Returns true if the two durations represent the same millisecond value
     def ==(other)
       @millis = other.to_millis
     end
@@ -116,6 +150,5 @@ module RhodaTime
     HOURS = 3600000
     DAYS = 86400000
     HOURS_IN_SECONDS = 3600
-
   end
 end
--- test/duration_test.rb
+++ test/duration_test.rb
@@ -21,6 +21,23 @@ module RhodaTime
       assert_equal(259200000, Duration.of_days(3).to_millis)
     end
 
+    def test_plus_minutes
+      assert_equal(2100000, Duration.of_minutes(30).plus_minutes(5).to_millis)
+      assert_equal(2100000, Duration.of_minutes(30).plus_minutes(5.234).to_millis)
+      assert_equal(1500000, Duration.of_minutes(30).plus_minutes(-5.234).to_millis)
+    end
+
+    def test_plus_seconds
+      assert_equal(1805000, Duration.of_minutes(30).plus_seconds(5).to_millis)
+      assert_equal(1795000, Duration.of_minutes(30).plus_seconds(-5).to_millis)
+      assert_equal(1805000, Duration.of_minutes(30).plus_seconds(5.234).to_millis)
+    end
+
+    def test_plus_hours
+      assert_equal(19800000, Duration.of_minutes(30).plus_hours(5).to_millis)
+      assert_equal(19800000, Duration.of_minutes(30).plus_hours(5.234).to_millis)
+    end
+
     def test_to_s
       assert_equal("PT23.456S", Duration.of_millis(23456).to_s)
       assert_equal("PT15M", Duration.of_minutes(15).to_s)
@@ -39,6 +56,12 @@ module RhodaTime
     def test_abs
       assert_equal(Duration.of_millis(23456), Duration.of_millis(-23456).abs)
       assert_equal(Duration.of_millis(23456), Duration.of_millis(23456).abs)
+    end
+
+    def test_negative?
+      assert_equal(true, Duration.of_millis(-2345).negative?)
+      assert_equal(false, Duration::ZERO.negative?)
+      assert_equal(false, Duration.of_millis(2345).negative?)
     end
 
     def test_equal
